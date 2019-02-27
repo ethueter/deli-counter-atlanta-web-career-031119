b008f9d5971e7f3673a70f8d5e27703270090a31
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-27 16:27:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.lenth >= 1 
+    counter = 1 
+    new_line = []
+    katz_deli.each do |name|
+      new_line.push(#{counter}. #{name})
+      counter += 1 
+    puts "The line is currently: #{new_line.join(" ")}"
+  else
+    puts "The line is currently empty."
+end
