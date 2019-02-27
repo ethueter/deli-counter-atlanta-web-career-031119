58ad4d569cbf452f42c1eb1b3109c7b6dfc4182f
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-27 17:12:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -28,7 +28,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.length >= 1 
-    puts "Now Serving #{katz_deli[0]}"
+    puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift!
   else 
   puts "There is nobody waiting to be served!"
