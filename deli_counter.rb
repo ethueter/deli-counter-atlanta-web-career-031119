374fdf8c71a63e134cf6e25b30ccd7ebc00db7d2
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-27 17:10:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -27,4 +27,10 @@ def take_a_number(katz_deli, name)
 end 
 
 def now_serving(katz_deli)
+  if katz_deli.length >= 1 
+    puts "Now Serving #{katz_deli[0]"
+    katz_deli.shift!
+  else 
+  puts "There is nobody waiting to be served!"
+end 
 end 
